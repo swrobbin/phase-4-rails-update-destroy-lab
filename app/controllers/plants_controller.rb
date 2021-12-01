0d1af7a2e2dcd17ed14e7bc265c68a01bd1f2445
Stephen <swrobbin@gmail.com>
2021-12-01 09:57:29 -0500

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,6 +17,26 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  # PATCH  /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+  # DELETE
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: not_found
+    end
+  end
 
   private
 
